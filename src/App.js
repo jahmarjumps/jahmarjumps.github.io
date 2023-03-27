4289fef6d5c0a14618ba165d6cc476f5d4f9a914
unknown <jahpereira01@gmail.com>
2023-03-27 15:53:20 -0400

    added image array and new game button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,74 @@
-import './App.css'
-function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
-}
 
-export default function MyApp() {
+import './App.css';
+import { useState} from 'react';
+
+const cardArray = [
+  { id: 1, 
+    src: "./images/batman.png"
+  },
+
+  {id: 2, 
+    src:"./images/cat.png"
+  },
+  {
+    id: 3,
+    src: "./images/harley.png"
+  },
+  {
+    id: 4,
+    src:"./images/riddler.png"
+  },
+  {id: 5,
+    src:"./images/joker.png"
+  },
+  {
+    id: 6,
+    src: "./images/scare.png"
+  }
+  
+];
+
+
+
+ 
+
+
+function App() {
+
+  const [cards, setCards] = useState([])
+  
+
+
+ /*randomizes cards */
+const shuffleCards= ()=> {
+  let shuffled = [...cardArray,...cardArray]
+  shuffled =shuffled.sort(()=> Math.random() - 0.5)
+  setCards(shuffled);
+  
+};
+console.log(cards)
+
+
   return (
     <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
+      <h1>Memory Game</h1>
+      <div className="grid">
+          <div className="card">
+            <div>
+              <img src = {require("./images/cover.png")} alt=""></img>
+            </div>
+          </div> 
+      </div>
+      <button onClick={shuffleCards}>New Game</button>  
     </div>
+     
+    
+
   );
+
+
+  
 }
+
+export default App;
+
